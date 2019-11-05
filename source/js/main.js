a982600a71861543a00838b18dbf01b243c57204
shockolada <shockolada@gmail.com>
2019-11-06 00:47:24 +0200

    05-11-19

--- source/js/main.js
+++ source/js/main.js
@@ -7,14 +7,40 @@ $(document).ready(function () {
 
   // SLIDERS
   var heroSlider = new Swiper('.hero-slider', {
-    // speed: 600,
+    init: false,
+    speed: 600,
     slidesPerView: 1,
+    loop: true,
     autoplay: {
-      // delay: 5000
+      delay: 5000
     },
     navigation: {
       nextEl: '.hero-slider__btn-next',
       prevEl: '.hero-slider__btn-prev',
     },
   });
+
+  var heroSlidesAll = $('.hero-slider__slide');
+  var heroSlidePrev = heroSlidesAll.last();
+  var heroSlideNext = heroSlidesAll.eq(1);
+  var heroSlideNextImg = heroSlideNext.css('background-image');
+  var heroSlidePrevImg = heroSlidePrev.css('background-image');
+  var heroSliderBtnNext = $('.hero-slider__btn-next .hero-slider__preview');
+  var heroSliderBtnPrev = $('.hero-slider__btn-prev .hero-slider__preview');
+
+  heroSliderBtnNext.css('background-image', heroSlideNextImg);
+  heroSliderBtnPrev.css('background-image', heroSlidePrevImg);
+
+  heroSlider.init();
+
+  heroSlider.on("slideChange", function () {
+    heroSlidePrev = $('.swiper-slide-prev');
+    heroSlideNext = $('.swiper-slide-next');
+    heroSlideNextImg = heroSlideNext.css('background-image');
+    heroSlidePrevImg = heroSlidePrev.css('background-image');
+    heroSliderBtnNext.css('background-image', heroSlideNextImg);
+    heroSliderBtnPrev.css('background-image', heroSlidePrevImg);
+    console.log('slideNext');
+  });
+
 });
