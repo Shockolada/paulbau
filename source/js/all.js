ac74f199c8ba6406af484261339f840f34066662
Shockolada <shockolada@gmail.com>
2019-11-05 18:40:55 +0300

    05-11-2019

--- source/js/all.js
+++ source/js/all.js
@@ -1,2 +1,3 @@
 @@include('./libs/jquery-3.4.1.js')
+@@include('./libs/swiper.js')
 @@include('./main.js')
--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,20 @@
-$(document).ready(function() {
-  $('.menu__toggle').click(function() {
+$(document).ready(function () {
+  $('.menu__toggle').click(function () {
     $(this).toggleClass('active');
     $('.menu').toggleClass('active');
+  });
+
+
+  // SLIDERS
+  var heroSlider = new Swiper('.hero-slider', {
+    // speed: 600,
+    slidesPerView: 1,
+    autoplay: {
+      // delay: 5000
+    },
+    navigation: {
+      nextEl: '.hero-slider__btn-next',
+      prevEl: '.hero-slider__btn-prev',
+    },
   });
 });
